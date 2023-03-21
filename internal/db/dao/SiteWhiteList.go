3b9102a3e98ae1de0891936ec3f7f1a25de2044a
Mmx233 <admin@mmxblog.com>
2023-03-21 20:13:54 +0800

    feat: SiteWhiteList dao 层操作函数

--- internal/db/dao/SiteWhiteList.go
+++ internal/db/dao/SiteWhiteList.go
@@ -1,7 +1,18 @@
 package dao
 
+import "gorm.io/gorm"
+
 type SiteWhiteList struct {
 	ID           uint `gorm:"primarykey"`
 	CreatedAt    int64
 	DomainSuffix string `gorm:"not null;uniqueIndex"`
+}
+
+func (a *SiteWhiteList) Insert(db *gorm.DB) error {
+	return db.Create(a).Error
+}
+
+func (a *SiteWhiteList) Get(db *gorm.DB) ([]SiteWhiteList, error) {
+	var t []SiteWhiteList
+	return t, db.Model(a).Find(&t).Error
 }
