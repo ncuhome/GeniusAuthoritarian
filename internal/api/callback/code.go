f583cbda7aed688a86cf7815579cf9fd1be2d76a
Mmx233 <admin@mmxblog.com>
2023-03-22 10:43:34 +0800

    feat: 站点过滤全局中间件

--- internal/api/callback/code.go
+++ internal/api/callback/code.go
@@ -1,9 +1,25 @@
 package callback
 
+const (
+	cErrForm uint8 = iota + 1
+	cErrSiteNotAllow
+	cErrDBOperation
+)
+
 var (
 	ErrForm = &Msg{
-		Code:       1,
+		Code:       cErrForm,
 		Msg:        "参数错误，请把输入截图发给前端同学",
 		HttpStatus: 400,
+	}
+	ErrSiteNotAllow = &Msg{
+		Code:       cErrSiteNotAllow,
+		Msg:        "该站点不在授权范围，请联系管理员添加",
+		HttpStatus: 403,
+	}
+	ErrDBOperation = &Msg{
+		Code:       cErrDBOperation,
+		Msg:        "数据库操作失败，请反馈后端同学修复",
+		HttpStatus: 500,
 	}
 )
--- internal/api/router/init.go
+++ internal/api/router/init.go
@@ -1,7 +1,10 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ncuhome/GeniusAuthoritarian/internal/api/middlewares"
+)
 
 func Api(G *gin.RouterGroup) {
-
+	G.Use(middlewares.SiteFilter)
 }
