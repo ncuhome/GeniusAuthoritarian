cf770cda3e2df734909368a1c7925796200bf49e
Mmx233 <admin@mmxblog.com>
2023-03-22 12:18:33 +0800

    sync: code: ldap

--- internal/pkg/ldap/ldap.go
+++ internal/pkg/ldap/ldap.go
@@ -3,15 +3,33 @@ package ldap
 import (
 	"github.com/go-ldap/ldap/v3"
 	"github.com/ncuhome/GeniusAuthoritarian/internal/global"
-	log "github.com/sirupsen/logrus"
 )
 
-var Conn *ldap.Conn
+type Client struct {
+	Conn *ldap.Conn
+}
 
-func init() {
-	var e error
-	Conn, e = ldap.DialURL(global.Config.Ldap.Addr)
-	if e != nil {
-		log.Fatalln("连接 ldap 服务失败:", e)
-	}
+func New() (Client, error) {
+	conn, e := ldap.DialURL(global.Config.Ldap.Addr)
+	return Client{
+		Conn: conn,
+	}, e
+}
+
+func (c Client) Close() {
+	c.Conn.Close()
+}
+
+func (c Client) UserIdentify(username string) string {
+	return "cn=" + username
+}
+
+// Bind 向该连接绑定账户，相当于登录
+func (c Client) Bind(username, password string) error {
+	return c.Conn.Bind(c.UserIdentify(username), password)
+}
+
+func (c Client) PasswordModify(username, oldPassword, newPassword string) error {
+	_, e := c.Conn.PasswordModify(ldap.NewPasswordModifyRequest(c.UserIdentify(username), oldPassword, newPassword))
+	return e
 }
