24e8c44773ff94168de9101d87a44052732c5927
Mmx233 <admin@mmxblog.com>
2023-03-24 17:25:42 +0800

    improve: 添加 import alias

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "path";
+
+const __dirname = path.resolve();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@api": path.resolve(__dirname, "src/network/api"),
+    }
+  }
 })
