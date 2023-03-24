6f88db160cc6728991fa9c39f03c648bbefbde10
Mmx233 <admin@mmxblog.com>
2023-03-24 18:22:18 +0800

    improve: 简化 feishu 模块，复用请求代码

--- pkg/feishu/feishu.go
+++ pkg/feishu/feishu.go
@@ -9,8 +9,8 @@ import (
 	"net/url"
 )
 
-func New(ClientID, Secret string, client *http.Client) *FsLogin {
-	return &FsLogin{
+func New(ClientID, Secret string, client *http.Client) *Fs {
+	return &Fs{
 		appID:  ClientID,
 		secret: Secret,
 		Http:   tool.NewHttpTool(client),
@@ -18,7 +18,7 @@ func New(ClientID, Secret string, client *http.Client) *FsLogin {
 	}
 }
 
-type FsLogin struct {
+type Fs struct {
 	appID  string
 	secret string
 	Http   *tool.Http
@@ -26,7 +26,30 @@ type FsLogin struct {
 	tenant *tenantTokenCache
 }
 
-func (f FsLogin) loadTenantAccessToken() (string, error) {
+func (f Fs) doRequest(Method string, data interface{}, opt *tool.DoHttpReq) error {
+	res, e := f.Http.Request(Method, opt)
+	if e != nil {
+		return e
+	}
+	defer res.Body.Close()
+
+	container := &Resp{
+		Data: data,
+	}
+	if e = json.NewDecoder(res.Body).Decode(container); e != nil {
+		return e
+	} else if container.Code != 0 {
+		return errors.New(container.Msg)
+	}
+
+	if res.StatusCode > 299 {
+		return fmt.Errorf("server return http status %d", res.StatusCode)
+	}
+
+	return nil
+}
+
+func (f Fs) loadTenantAccessToken() (string, error) {
 	if token, exist := f.tenant.Load(); exist {
 		return token, nil
 	}
@@ -59,7 +82,7 @@ func (f FsLogin) loadTenantAccessToken() (string, error) {
 	return data.TenantAccessToken, nil
 }
 
-func (f FsLogin) LoginLink(state string) string {
+func (f Fs) LoginLink(state string) string {
 	return fmt.Sprintf(
 		"https://open.feishu.cn/open-apis/authen/v1/user_auth_page_beta?app_id=%s&redirect_uri=https%%3A%%2F%%2Fv.ncuos.com%%2Fapi%%2Fpublic%%2Flogin%%2Ffeishu%%2F&state=%s",
 		f.appID,
@@ -67,13 +90,15 @@ func (f FsLogin) LoginLink(state string) string {
 	)
 }
 
-func (f FsLogin) GetUser(code string) (*FsUser, error) {
+func (f Fs) GetUser(code string) (*FsUser, error) {
 	tenantToken, e := f.loadTenantAccessToken()
 	if e != nil {
 		return nil, e
 	}
 
-	res, e := f.Http.PostRequest(&tool.DoHttpReq{
+	var data FsUser
+	data.fs = f
+	return &data, f.doRequest("POST", &data, &tool.DoHttpReq{
 		Url: "https://open.feishu.cn/open-apis/authen/v1/access_token",
 		Header: map[string]interface{}{
 			"Authorization": "Bearer " + tenantToken,
@@ -84,22 +109,14 @@ func (f FsLogin) GetUser(code string) (*FsUser, error) {
 			"code":       code,
 		},
 	})
+}
+
+/*func (f Fs) LoadDepartmentList() (map[string]interface{}, error) {
+	tenantToken, e := f.loadTenantAccessToken()
 	if e != nil {
 		return nil, e
 	}
-	defer res.Body.Close()
 
-	var data OAuth2AccessTokenResp
-	if e = json.NewDecoder(res.Body).Decode(&data); e != nil {
-		return nil, e
-	} else if data.Code != 0 {
-		return nil, errors.New(data.Msg)
-	}
-
-	if res.StatusCode > 299 {
-		return nil, fmt.Errorf("server return http status %d", res.StatusCode)
-	}
-
-	data.Data.Http = f.Http
-	return &data.Data, nil
+	return nil, nil
 }
+*/
--- pkg/feishu/models.go
+++ pkg/feishu/models.go
@@ -1,5 +1,11 @@
 package feishu
 
+type Resp struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
 type TenantAccessTokenResp struct {
 	Code              int    `json:"code"`
 	Msg               string `json:"msg"`
@@ -7,53 +13,43 @@ type TenantAccessTokenResp struct {
 	Expire            int64  `json:"expire"`
 }
 
-type OAuth2AccessTokenResp struct {
-	Code int    `json:"code"`
-	Msg  string `json:"msg"`
-	Data FsUser `json:"data"`
-}
-
 type UserInfoResp struct {
-	Code int `json:"code"`
-	Data struct {
-		User struct {
-			Avatar struct {
-				Avatar240    string `json:"avatar_240"`
-				Avatar640    string `json:"avatar_640"`
-				Avatar72     string `json:"avatar_72"`
-				AvatarOrigin string `json:"avatar_origin"`
-			} `json:"avatar"`
-			City            string        `json:"city"`
-			Country         string        `json:"country"`
-			CustomAttrs     []interface{} `json:"custom_attrs"`
-			DepartmentIds   []string      `json:"department_ids"`
-			Description     string        `json:"description"`
-			EmployeeNo      string        `json:"employee_no"`
-			EmployeeType    int           `json:"employee_type"`
-			EnName          string        `json:"en_name"`
-			EnterpriseEmail string        `json:"enterprise_email"`
-			Gender          int           `json:"gender"`
-			IsTenantManager bool          `json:"is_tenant_manager"`
-			JobTitle        string        `json:"job_title"`
-			JoinTime        int           `json:"join_time"`
-			MobileVisible   bool          `json:"mobile_visible"`
-			Name            string        `json:"name"`
-			OpenId          string        `json:"open_id"`
-			Orders          []struct {
-				DepartmentId    string `json:"department_id"`
-				DepartmentOrder int    `json:"department_order"`
-				UserOrder       int    `json:"user_order"`
-			} `json:"orders"`
-			Status struct {
-				IsActivated bool `json:"is_activated"`
-				IsExited    bool `json:"is_exited"`
-				IsFrozen    bool `json:"is_frozen"`
-				IsResigned  bool `json:"is_resigned"`
-				IsUnjoin    bool `json:"is_unjoin"`
-			} `json:"status"`
-			UnionId     string `json:"union_id"`
-			WorkStation string `json:"work_station"`
-		} `json:"user"`
-	} `json:"data"`
-	Msg string `json:"msg"`
+	User struct {
+		Avatar struct {
+			Avatar240    string `json:"avatar_240"`
+			Avatar640    string `json:"avatar_640"`
+			Avatar72     string `json:"avatar_72"`
+			AvatarOrigin string `json:"avatar_origin"`
+		} `json:"avatar"`
+		City            string        `json:"city"`
+		Country         string        `json:"country"`
+		CustomAttrs     []interface{} `json:"custom_attrs"`
+		DepartmentIds   []string      `json:"department_ids"`
+		Description     string        `json:"description"`
+		EmployeeNo      string        `json:"employee_no"`
+		EmployeeType    int           `json:"employee_type"`
+		EnName          string        `json:"en_name"`
+		EnterpriseEmail string        `json:"enterprise_email"`
+		Gender          int           `json:"gender"`
+		IsTenantManager bool          `json:"is_tenant_manager"`
+		JobTitle        string        `json:"job_title"`
+		JoinTime        int           `json:"join_time"`
+		MobileVisible   bool          `json:"mobile_visible"`
+		Name            string        `json:"name"`
+		OpenId          string        `json:"open_id"`
+		Orders          []struct {
+			DepartmentId    string `json:"department_id"`
+			DepartmentOrder int    `json:"department_order"`
+			UserOrder       int    `json:"user_order"`
+		} `json:"orders"`
+		Status struct {
+			IsActivated bool `json:"is_activated"`
+			IsExited    bool `json:"is_exited"`
+			IsFrozen    bool `json:"is_frozen"`
+			IsResigned  bool `json:"is_resigned"`
+			IsUnjoin    bool `json:"is_unjoin"`
+		} `json:"status"`
+		UnionId     string `json:"union_id"`
+		WorkStation string `json:"work_station"`
+	} `json:"user"`
 }
