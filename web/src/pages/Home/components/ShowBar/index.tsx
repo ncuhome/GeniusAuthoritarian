16375115a74d3809ab429bf11d19f02dda594053
Mmx233 <admin@mmxblog.com>
2023-04-03 13:03:43 +0800

    feat: 登录页 logo

---
+++ web/src/pages/Home/components/ShowBar/index.tsx
@@ -0,0 +1,26 @@
+import {FC} from "react";
+import logo from '@/assets/img/logo-lg.png'
+
+import {Stack, Box} from "@mui/material";
+
+export const ShowBar: FC = () => {
+    return <Stack sx={{
+        height: '100%',
+        width: '100%',
+        boxShadow: '0 0 15px 0 #343434',
+        backgroundColor: '#343434'
+    }}>
+        <Box sx={{
+            padding: '2.5rem 4rem',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            '&>img': {
+                maxHeight: '100%',
+                maxWidth: '100%',
+                width: '15rem'
+            }
+        }}><img src={logo} alt={"家园工作室"}/></Box>
+    </Stack>
+}
+export default ShowBar
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,8 +1,27 @@
 import {FC} from "react";
 
+import {Box, Stack} from "@mui/material"
+import {ShowBar} from "./components";
+
 export const Home:FC = ()=>{
-    return <>
-    </>
+    return <Stack flexDirection={'row'} sx={{
+        width: '100%',
+        height: '100%',
+        '&>div': {
+            height: '100%',
+        }
+    }}>
+        <Box sx={{
+            width: '40%'
+        }}>
+            <ShowBar/>
+        </Box>
+        <Box sx={{
+            flexGrow: 1
+        }}>
+
+        </Box>
+    </Stack>
 }
 
 export default Home
