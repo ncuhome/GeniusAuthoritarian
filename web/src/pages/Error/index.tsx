05e287417670e43e5c3aea1e62bf225f0d29d192
Mmx233 <admin@mmxblog.com>
2023-04-08 21:35:19 +0800

    feat: 错误页显示错误内容

--- web/src/pages/Error/index.tsx
+++ web/src/pages/Error/index.tsx
@@ -1,10 +1,14 @@
 import {FC} from "react";
+import {useQuery} from "@hooks";
 import './styles.css'
 
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import {ClearRounded} from '@mui/icons-material';
 
 export const Error:FC = ()=>{
+    const [title]=useQuery('title', '未知错误')
+    const [content]=useQuery('content','')
+
     return <Box sx={{
         height: '100%',
         width: '100%',
@@ -14,15 +18,28 @@ export const Error:FC = ()=>{
     }}>
         <Box sx={{
             display: 'flex',
-            "& svg": {
-                borderStyle: 'solid',
-                borderRadius: '50%',
-                fontSize: '12rem',
-                animation: 'error-page-fork .5s ease',
-                animationFillMode: 'forwards'
-            }
+            flexDirection: 'column',
+            textAlign: 'center',
+            justifyContent: 'center',
         }}>
-            <ClearRounded/>
+            <Box sx={{
+                height: '15.5rem',
+                "& svg": {
+                    borderStyle: 'solid',
+                    borderRadius: '50%',
+                    fontSize: '12rem',
+                    animation: 'error-page-fork .5s ease',
+                    animationFillMode: 'forwards',
+                }
+            }}>
+                <ClearRounded/>
+            </Box>
+            <Typography variant={'h4'}>{title}</Typography>
+            {content?<Typography sx={{
+                color: '#999',
+                marginTop: '0.4rem',
+                wordBreak: 'break-all',
+            }}>{content}</Typography>:undefined}
         </Box>
     </Box>
 }
