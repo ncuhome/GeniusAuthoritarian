aaddda7c3bc587baab69a178e03e80bd314ddeb4
Mmx233 <admin@mmxblog.com>
2023-04-15 17:29:15 +0800

    feat: 飞书登录接口调用函数

--- web/src/network/api/v1/login.ts
+++ web/src/network/api/v1/login.ts
@@ -3,7 +3,7 @@ import { apiV1 } from "@api/base";
 export async function GetFeishuLoginUrl(target: string): Promise<string> {
   const {
     data: {
-      data: { url },
+      data: {url},
     },
   } = await apiV1.get("public/login/feishu/link", {
     params: {
@@ -11,4 +11,17 @@ export async function GetFeishuLoginUrl(target: string): Promise<string> {
     },
   });
   return url;
+}
+
+export async function FeishuLogin(code: string): Promise<string> {
+  const {
+    data: {
+      data: {token},
+    },
+  } = await apiV1.post("public/login/feishu/", {
+    data: {
+      code,
+    },
+  });
+  return token
 }
