c9c8ad279d9e2b05685b4864f19392a975f9d0be
Mmx233 <admin@mmxblog.com>
2023-04-15 20:42:58 +0800

    improve: 尝试优化首页移动端样式

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+import {FC} from "react";
+import './styles.css'
 
 import { Box, Stack } from "@mui/material";
 import { ShowBar, LoginForm } from "./components";
@@ -16,20 +17,21 @@ export const Home: FC = () => {
       }}
     >
       <Box
-        sx={{
-          width: "20rem",
-        }}
+          className={"show-bar"}
+          sx={{
+              width: "20rem",
+          }}
       >
         <ShowBar />
       </Box>
       <Box
         sx={{
-          flexGrow: 1,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "2rem 3rem",
-          boxSizing: "border-box",
+            flexGrow: 1,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "2rem 3%",
+            boxSizing: "border-box",
         }}
       >
         <LoginForm />
