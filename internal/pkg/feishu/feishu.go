e472e64ffe92da7fba77587f34a76a595e40b73c
Mmx233 <admin@mmxblog.com>
2023-04-26 22:23:07 +0800

    improve: 只在数据库为空时在启动时同步数据

--- internal/pkg/feishu/feishu.go
+++ internal/pkg/feishu/feishu.go
@@ -1,6 +1,7 @@
 package feishu
 
 import (
+	"github.com/ncuhome/GeniusAuthoritarian/internal/db/dao"
 	"github.com/ncuhome/GeniusAuthoritarian/internal/global"
 	"github.com/ncuhome/GeniusAuthoritarian/pkg/feishuApi"
 	"github.com/ncuhome/GeniusAuthoritarian/tools"
@@ -8,17 +9,29 @@ import (
 )
 
 func init() {
-	if e := DepartmentSync(); e != nil {
-		log.Fatalf("同步飞书部门失败: %v", e)
+	var count int64
+	e := dao.DB.Model(&dao.Group{}).Count(&count).Error
+	if e != nil {
+		log.Fatalln(e)
 	}
-	if e := UserSync(); e != nil {
-		log.Fatalf("同步飞书用户失败: %v", e)
+	if count == 0 {
+		if e = DepartmentSync(); e != nil {
+			log.Fatalf("同步飞书部门失败: %v", e)
+		}
+	}
+	if e = dao.DB.Model(&dao.User{}).Count(&count).Error; e != nil {
+		log.Fatalln(e)
+	}
+	if count == 0 {
+		if e = UserSync(); e != nil {
+			log.Fatalf("同步飞书用户失败: %v", e)
+		}
 	}
 
-	if e := AddDepartmentSyncCron("0 5 * * *"); e != nil {
+	if e = AddDepartmentSyncCron("0 5 * * *"); e != nil {
 		log.Fatalf("添加定时同步飞书部门任务失败: %v", e)
 	}
-	if e := AddUserSyncCron("30 5 * * *"); e != nil {
+	if e = AddUserSyncCron("30 5 * * *"); e != nil {
 		log.Fatalf("添加定时同步飞书用户任务失败: %v", e)
 	}
 }
