a209246bec67bcd8f91f52ef5d61c6c82fe90d67
Mmx233 <admin@mmxblog.com>
2023-04-30 19:41:44 +0800

    improve: 调试模式前端请求转发改用 Gateway/v2

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/Mmx233/EnvConfig v0.0.2
+	github.com/Mmx233/Gateway/v2 v2.0.3
 	github.com/Mmx233/config v0.1.0
 	github.com/Mmx233/daoUtil v0.6.1
 	github.com/Mmx233/tool v0.6.9
@@ -52,7 +53,7 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
--- internal/router/web.go
+++ internal/router/web.go
@@ -1,9 +1,8 @@
 package router
 
 import (
-	"github.com/Mmx233/tool"
+	gateway "github.com/Mmx233/Gateway/v2"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 	"github.com/ncuhome/GeniusAuthoritarian/internal/global"
 	"github.com/ncuhome/GeniusAuthoritarian/tools"
 	"github.com/ncuhome/GeniusAuthoritarian/web"
@@ -18,94 +17,16 @@ func frontendRouterCheck(c *gin.Context) bool {
 	return !strings.HasPrefix(c.Request.URL.Path, "/api")
 }
 
-func frontendLocalDevHandler(c *gin.Context) {
-	if !frontendRouterCheck(c) {
-		return
-	}
-
-	c.Header("Access-Control-Allow-Origin", "*")
-
-	if c.IsWebsocket() {
-		connClient, e := tools.UpgradeDevWs(c)
-		if e != nil {
-			return
-		}
-
-		var headers = make(http.Header)
-		headers.Set("Sec-Websocket-Protocol", c.Request.Header.Get("Sec-Websocket-Protocol"))
-		for k, v := range c.Request.Header {
-			if strings.HasPrefix(k, "Accept") {
-				headers.Set(k, v[0])
-			}
-		}
-		connServer, res, e := websocket.DefaultDialer.Dial("ws://127.0.0.1:5173"+c.Request.URL.Path, headers)
-		if e != nil {
-			connClient.Close()
-			return
-		}
-
-		c.AbortWithStatus(res.StatusCode)
-
-		go func() {
-			defer connServer.Close()
-			for {
-				t, p, e := connClient.ReadMessage()
-				if e != nil {
-					return
-				}
-				_ = connServer.WriteMessage(t, p)
-			}
-		}()
-
-		go func() {
-			defer connClient.Close()
-			for {
-				t, p, e := connServer.ReadMessage()
-				if e != nil {
-					return
-				}
-				_ = connClient.WriteMessage(t, p)
-			}
-		}()
-		return
-	}
-
-	query := make(map[string]interface{}, len(c.Request.URL.Query()))
-	for k, v := range c.Request.URL.Query() {
-		query[k] = v[0]
-	}
-	var header = make(map[string]interface{})
-	for k, v := range c.Request.Header {
-		if strings.HasPrefix(k, "Accept") {
-			header[k] = v[0]
-		}
-	}
-	res, e := tools.Http.Request(c.Request.Method, &tool.DoHttpReq{
-		Url:    "http://127.0.0.1:5173" + c.Request.URL.Path,
-		Header: header,
-		Query:  query,
-		Body:   c.Request.Body,
-	})
-	if e != nil {
-		c.AbortWithStatus(502)
-		return
-	}
-	defer res.Body.Close()
-
-	for k, v := range res.Header {
-		if strings.HasPrefix(k, "Content-") {
-			c.Header(k, v[0])
-		}
-	}
-	c.Status(res.StatusCode)
-
-	_, e = io.Copy(c.Writer, res.Body)
-	if e != nil {
-		c.AbortWithStatus(502)
-		return
-	}
-	c.Abort()
-}
+var frontendLocalDevHandler = gateway.Proxy(&gateway.ApiConf{
+	Addr:      "localhost:5173",
+	Transport: tools.Http.Client.Transport,
+	ErrorHandler: func(_ http.ResponseWriter, _ *http.Request, err error) {
+		log.Warnf("调试页面请求转发失败: %v", err)
+	},
+	AllowRequest: func(c *gin.Context) bool {
+		return frontendRouterCheck(c)
+	},
+})
 
 func frontendProductionHandler() gin.HandlerFunc {
 	fe, e := fs.Sub(web.FS, "dist")
