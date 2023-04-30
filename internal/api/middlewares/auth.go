727da434eccd278e20e2ec36b15fa06b2c6c1f6f
Mmx233 <admin@mmxblog.com>
2023-04-30 22:31:49 +0800

    feat: 用户鉴权中间件

---
+++ internal/api/middlewares/auth.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ncuhome/GeniusAuthoritarian/internal/api/callback"
+	"github.com/ncuhome/GeniusAuthoritarian/internal/pkg/jwt"
+)
+
+func UserAuth(c *gin.Context) {
+	token := c.GetHeader("Authorization")
+	if token == "" {
+		callback.Error(c, nil, callback.ErrUnauthorized)
+		return
+	}
+
+	claims, valid, e := jwt.ParseUserToken(token)
+	if e != nil || !valid {
+		callback.Error(c, e, callback.ErrUnauthorized)
+		return
+	}
+
+	c.Set("UID", claims.ID)
+}
--- internal/api/router/v1.go
+++ internal/api/router/v1.go
@@ -3,8 +3,10 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/ncuhome/GeniusAuthoritarian/internal/api/router/public"
+	"github.com/ncuhome/GeniusAuthoritarian/internal/api/router/user"
 )
 
 func ApiV1(G *gin.RouterGroup) {
 	public.Router(G.Group("public"))
+	user.Router(G.Group("user"))
 }
