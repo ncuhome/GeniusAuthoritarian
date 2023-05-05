3a4b90baa82bf007924f9915a72a3dc1cebf3758
Mmx233 <admin@mmxblog.com>
2023-05-05 14:27:28 +0800

    feat: oss 包基本操作方法

--- pkg/aliyun/oss/oss.go
+++ pkg/aliyun/oss/oss.go
@@ -1,6 +1,11 @@
 package oss
 
-import "github.com/aliyun/aliyun-oss-go-sdk/oss"
+import (
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"io"
+	"path"
+	"time"
+)
 
 func NewRoot(endpoint, accessKey, secretKey, bucket string) (*Storage, error) {
 	c, e := oss.New(endpoint, accessKey, secretKey)
@@ -21,4 +26,22 @@ type Storage struct {
 	path string
 	c    *oss.Client
 	b    *oss.Bucket
+}
+
+func (a *Storage) NewDir(childDir string) *Storage {
+	var dir = *a
+	dir.path = path.Join(dir.path, childDir)
+	return &dir
+}
+
+func (a *Storage) Put(name string, reader io.Reader, options ...oss.Option) error {
+	return a.b.PutObject(path.Join(a.path, name), reader, options...)
+}
+
+func (a *Storage) Del(name string, options ...oss.Option) error {
+	return a.b.DeleteObject(path.Join(a.path, name), options...)
+}
+
+func (a *Storage) SignGetUrl(name string, exprIn time.Duration, options ...oss.Option) (string, error) {
+	return a.b.SignURL(path.Join(a.path, name), oss.HTTPGet, int64(exprIn.Seconds()), options...)
 }
