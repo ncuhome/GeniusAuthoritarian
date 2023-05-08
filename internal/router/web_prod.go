69f6e551e50ed1a66eae606450d0d6f2787ab24e
Mmx233 <admin@mmxblog.com>
2023-05-08 14:48:48 +0800

    feat: 尝试在生产模式下对静态文件返回 304

--- internal/router/web_prod.go
+++ internal/router/web_prod.go
@@ -3,6 +3,8 @@
 package router
 
 import (
+	"crypto/md5"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/ncuhome/GeniusAuthoritarian/web"
 	log "github.com/sirupsen/logrus"
@@ -10,25 +12,31 @@ import (
 	"io/fs"
 	"net/http"
 	"strings"
+	"sync"
 )
+
+func calcEtag(f fs.File) (string, error) {
+	defer f.Close()
+
+	d, e := io.ReadAll(f)
+	if e != nil {
+		return "", e
+	}
+
+	hash := md5.New()
+	hash.Write(d)
+	return fmt.Sprintf("%x", hash), nil
+}
 
 func frontendHandler() gin.HandlerFunc {
 	fe, e := fs.Sub(web.FS, "dist")
 	if e != nil {
 		log.Fatalln(e)
 	}
-	file, err := fe.Open("index.html")
-	if e != nil {
-		log.Fatalln(e)
-	}
-	fileContentBytes, e := io.ReadAll(file)
-	if err != nil {
-		log.Fatalln(e)
-	}
-	_ = file.Close()
-	index := string(fileContentBytes)
-
 	fileServer := http.StripPrefix("/", http.FileServer(http.FS(fe)))
+
+	const IndexPath = "/index.html"
+	var etags = &sync.Map{}
 
 	return func(c *gin.Context) {
 		if !frontendRouterCheck(c) {
@@ -37,16 +45,35 @@ func frontendHandler() gin.HandlerFunc {
 
 		f, e := fe.Open(strings.TrimPrefix(c.Request.URL.Path, "/"))
 		if e != nil {
-			if _, ok := e.(*fs.PathError); ok {
-				c.Header("Content-Type", "text/html")
-				c.String(200, index)
-				c.Abort()
+			if _, ok := e.(*fs.PathError); ok && c.Request.URL.Path != IndexPath {
+				c.Request.URL.Path = IndexPath
+			} else {
+				log.Errorln("加载 embed fs 失败:", e)
+				c.AbortWithStatus(500)
 				return
 			}
-			c.AbortWithStatus(500)
+		}
+
+		etag, ok := etags.Load(c.Request.URL.Path)
+		if ok {
+			_ = f.Close()
+		} else {
+			etag, e = calcEtag(f)
+			if e != nil {
+				log.Errorln("计算 etag 失败:", e)
+				c.AbortWithStatus(500)
+				return
+			}
+			etags.Store(c.Request.URL.Path, etag)
+		}
+
+		c.Header("Etag", etag.(string))
+
+		if c.GetHeader("If-none-match") == etag.(string) {
+			c.AbortWithStatus(304)
 			return
 		}
-		_ = f.Close()
+
 		fileServer.ServeHTTP(c.Writer, c.Request)
 		c.Abort()
 	}
