7d1a3381b4ee965b74414e3c85334df8a5e1fe1d
Mmx233 <admin@mmxblog.com>
2023-05-08 15:08:49 +0800

    fix: 回退 index 页 serve 逻辑

--- internal/router/web_prod.go
+++ internal/router/web_prod.go
@@ -15,7 +15,13 @@ import (
 	"sync"
 )
 
-func calcEtag(f fs.File) (string, error) {
+func calcEtag(d []byte) string {
+	hash := md5.New()
+	hash.Write(d)
+	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+func calcFsEtag(f fs.File) (string, error) {
 	defer f.Close()
 
 	d, e := io.ReadAll(f)
@@ -23,9 +29,7 @@ func calcEtag(f fs.File) (string, error) {
 		return "", e
 	}
 
-	hash := md5.New()
-	hash.Write(d)
-	return fmt.Sprintf("%x", hash.Sum(nil)), nil
+	return calcEtag(d), nil
 }
 
 func frontendHandler() gin.HandlerFunc {
@@ -33,22 +37,37 @@ func frontendHandler() gin.HandlerFunc {
 	if e != nil {
 		log.Fatalln(e)
 	}
+
+	file, err := fe.Open("index.html")
+	if e != nil {
+		log.Fatalln(e)
+	}
+	fileContentBytes, e := io.ReadAll(file)
+	if err != nil {
+		log.Fatalln(e)
+	}
+	_ = file.Close()
+	index := string(fileContentBytes)
+	indexEtag := calcEtag(fileContentBytes)
+
 	fileServer := http.StripPrefix("/", http.FileServer(http.FS(fe)))
 
-	const IndexPath = "/index.html"
 	var etags = &sync.Map{}
+	etags.Store("/index.html", indexEtag)
 
 	return func(c *gin.Context) {
 		if !frontendRouterCheck(c) {
 			return
 		}
 
-	checkFile:
 		f, e := fe.Open(strings.TrimPrefix(c.Request.URL.Path, "/"))
 		if e != nil {
-			if _, ok := e.(*fs.PathError); ok && c.Request.URL.Path != IndexPath {
-				c.Request.URL.Path = IndexPath
-				goto checkFile
+			if _, ok := e.(*fs.PathError); ok {
+				c.Header("Content-Type", "text/html")
+				c.Header("Etag", indexEtag)
+				c.String(200, index)
+				c.Abort()
+				return
 			} else {
 				log.Errorln("加载 embed fs 失败:", e)
 				c.AbortWithStatus(500)
@@ -60,7 +79,7 @@ func frontendHandler() gin.HandlerFunc {
 		if ok {
 			_ = f.Close()
 		} else {
-			etag, e = calcEtag(f)
+			etag, e = calcFsEtag(f)
 			if e != nil {
 				log.Errorln("计算 etag 失败:", e)
 				c.AbortWithStatus(500)
@@ -71,7 +90,7 @@ func frontendHandler() gin.HandlerFunc {
 
 		c.Header("Etag", etag.(string))
 
-		if c.GetHeader("If-none-match") == etag.(string) {
+		if c.GetHeader("If-None-Match") == etag.(string) {
 			c.AbortWithStatus(304)
 			return
 		}
