9d8a0db35dc62405b47337cc1b53e8398096b2d1
Mmx233 <admin@mmxblog.com>
2023-05-10 22:45:32 +0800

    chore: 更新登录回调请求函数

--- web/src/network/api/v1/login.ts
+++ web/src/network/api/v1/login.ts
@@ -1,5 +1,17 @@
 import { apiV1 } from "@api/base";
 
+export async function GetLoginUrl(
+  thirdParty: string,
+  appCode: string = ""
+): Promise<string> {
+  const {
+    data: {
+      data: { url },
+    },
+  } = await apiV1.get(`public/login/${thirdParty}/link/${appCode}`);
+  return url;
+}
+
 export async function UserLogin(token: string): Promise<string> {
   const {
     data: {
@@ -11,50 +23,16 @@ export async function UserLogin(token: string): Promise<string> {
   return authToken;
 }
 
-export async function GetFeishuLoginUrl(appCode: string): Promise<string> {
-  const {
-    data: {
-      data: { url },
-    },
-  } = await apiV1.get("public/login/feishu/link", {
-    params: {
-      appCode,
-    },
-  });
-  return url;
-}
-
-export async function FeishuLogin(code: string, appCode: string): Promise<string> {
+export async function Login(
+  thirdParty: string,
+  code: string,
+  appCode: string
+): Promise<string> {
   const {
     data: {
       data: { token, callback: callbackUrl },
     },
-  } = await apiV1.post("public/login/feishu/", {
-    code,
-    appCode,
-  });
-  return callbackUrl;
-}
-
-export async function GetDingTalkLoginUrl(appCode: string): Promise<string> {
-  const {
-    data: {
-      data: { url },
-    },
-  } = await apiV1.get("public/login/dingTalk/link", {
-    params: {
-      appCode,
-    },
-  });
-  return url;
-}
-
-export async function DingTalkLogin(code: string, appCode: string): Promise<string> {
-  const {
-    data: {
-      data: { token, callback: callbackUrl },
-    },
-  } = await apiV1.post("public/login/dingTalk/", {
+  } = await apiV1.post(`public/login/${thirdParty}/${appCode}`, {
     code,
     appCode,
   });
