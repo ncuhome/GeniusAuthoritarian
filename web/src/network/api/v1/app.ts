fc6bb66e446cda488f0e64e1adac6f16fc21cc38
Mmx233 <admin@mmxblog.com>
2023-05-10 16:28:38 +0800

    feat: app 信息请求函数

---
+++ web/src/network/api/v1/app.ts
@@ -0,0 +1,17 @@
+import { apiV1 } from "@api/base";
+
+export type AppInfo = {
+  name: string;
+  createdAt: number;
+};
+
+export async function GetAppInfo(appCode: string): Promise<AppInfo> {
+  const {
+    data: { data },
+  } = await apiV1.get("public/app/", {
+    params: {
+      appCode,
+    },
+  });
+  return data;
+}
