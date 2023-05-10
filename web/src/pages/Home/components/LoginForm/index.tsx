44f0e4ec6904e3221ba26d002078a340ed443482
Mmx233 <admin@mmxblog.com>
2023-05-10 17:07:31 +0800

    feat: 登陆时显示目标 app 信息

--- web/src/pages/Home/components/LoginForm/index.tsx
+++ web/src/pages/Home/components/LoginForm/index.tsx
@@ -1,14 +1,15 @@
-import { FC } from "react";
-import { createUseQuery, useMount } from "@hooks";
+import { FC, useState } from "react";
+import { createUseQuery, useMount, useInterval } from "@hooks";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import feishuLogo from "@/assets/img/login/feishu.png";
 import dingLogo from "@/assets/img/login/ding.png";
 
-import { Stack, Box, Typography, List, Paper } from "@mui/material";
+import { Stack, Box, Typography, List, Paper, Skeleton } from "@mui/material";
 import { LoginItem } from "./components";
 
 import { GetFeishuLoginUrl, GetDingTalkLoginUrl } from "@api/v1/login";
+import { GetAppInfo, AppInfo } from "@api/v1/app";
 
 import { useUser } from "@store";
 
@@ -20,6 +21,9 @@ export const LoginForm: FC = () => {
 
   const token = useUser((state) => state.token);
 
+  const [appInfo, setAppInfo] = useState<AppInfo | null>(null);
+  const [onRequestAppInfo, setOnRequestAppInfo] = useState(true);
+
   async function goFeishuLogin() {
     try {
       const url = await GetFeishuLoginUrl(appCode);
@@ -28,6 +32,7 @@ export const LoginForm: FC = () => {
       if (msg) toast.error(msg as string);
     }
   }
+
   async function goDingTalkLogin() {
     try {
       const url = await GetDingTalkLoginUrl(appCode);
@@ -37,6 +42,19 @@ export const LoginForm: FC = () => {
     }
   }
 
+  async function loadAppInfo() {
+    setOnRequestAppInfo(true);
+    try {
+      const data = await GetAppInfo(appCode);
+      setAppInfo(data);
+    } catch ({ msg }) {
+      if (msg) toast.error(msg as string);
+    }
+    setOnRequestAppInfo(false);
+  }
+
+  useInterval(loadAppInfo, !appInfo && !onRequestAppInfo ? 2000 : null);
+
   useMount(() => {
     if (token && !appCode) nav("/user");
     switch (true) {
@@ -44,6 +62,8 @@ export const LoginForm: FC = () => {
         goFeishuLogin();
         break;
     }
+
+    loadAppInfo();
   });
 
   return (
@@ -65,15 +85,57 @@ export const LoginForm: FC = () => {
         }}
         justifyContent={"center"}
       >
-        <Typography
-          variant={"h5"}
+        <Stack
+          alignItems={"center"}
           sx={{
             marginBottom: "2rem",
           }}
         >
-          登录
-        </Typography>
+          {appInfo ? (
+            <>
+              <Typography variant={"h6"}>
+                <Typography
+                  variant={"h6"}
+                  display={"inline"}
+                  fontWeight={"bold"}
+                  letterSpacing={"1px"}
+                >
+                  {appInfo.name}
+                </Typography>
+                {` 请求登录你的帐号`}
+              </Typography>
+              <Typography
+                variant={"subtitle1"}
+                sx={{ opacity: 0.6 }}
+              >{`aka. ${appInfo.host}`}</Typography>
+            </>
+          ) : (
+            <>
+              <Skeleton
+                variant={"text"}
+                sx={{
+                  fontSize: "1.25rem",
+                  lineHeight: 1.6,
+                  fontWeight: 400,
+                  width: "75%",
+                }}
+              />
+              <Skeleton
+                variant={"text"}
+                sx={{
+                  fontSize: "1rem",
+                  lineHeight: 1.75,
+                  fontWeight: 400,
+                  width: "30%",
+                }}
+              />
+            </>
+          )}
+        </Stack>
 
+        <Typography variant={"body2"} sx={{ opacity: 0.6, textAlign: "left" }}>
+          请选择你的登录方式:
+        </Typography>
         <List>
           <LoginItem logo={feishuLogo} text={"飞书"} onClick={goFeishuLogin} />
           <LoginItem
