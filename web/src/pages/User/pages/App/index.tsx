b53ace569c7e5acc54277a9facdb07059a518fd0
Mmx233 <admin@mmxblog.com>
2023-05-13 00:42:52 +0800

    feat: user/app 页载入 app 数据

--- web/src/pages/User/pages/App/index.tsx
+++ web/src/pages/User/pages/App/index.tsx
@@ -1,9 +1,36 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { useLoadingToast, useMount, useInterval } from "@hooks";
 
 import { Block } from "@/pages/User/components";
 import { Container } from "@mui/material";
 
+import { GetOwnedAppList } from "@api/v1/user/app";
+
+import { useUser } from "@store";
+
 export const App: FC = () => {
+  const apps = useUser((state) => state.apps);
+  const setApps = useUser((state) => state.setState("apps"));
+
+  const [onRequest, setOnRequest] = useState(true);
+  const [loadAppsToast, closeAppsToast] = useLoadingToast();
+
+  async function loadApps() {
+    try {
+      const data = await GetOwnedAppList();
+      setApps(data);
+      closeAppsToast();
+    } catch ({ msg }) {
+      if (msg) loadAppsToast(msg as string);
+    }
+  }
+
+  useInterval(loadApps, !apps && !onRequest ? 2000 : null);
+  useMount(() => {
+    if (!apps) loadApps();
+    else setOnRequest(false);
+  });
+
   return (
     <Container>
       <Block title={"New"}></Block>
