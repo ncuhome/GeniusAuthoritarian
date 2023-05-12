1a2033bf6b84da78b8392d6bbde7a0975eabd035
Mmx233 <admin@mmxblog.com>
2023-05-12 23:47:07 +0800

    feat: app 接口请求实现

---
+++ web/src/network/api/v1/user/app.ts
@@ -0,0 +1,30 @@
+import { apiV1User } from "@api/v1/user/base";
+
+export type App = {
+  id: number;
+  name: string;
+  appCode: string;
+  permitAllGroup: boolean;
+};
+
+export async function GetOwnedAppList(): Promise<App[]> {
+  const {
+    data: { data },
+  } = await apiV1User.get("app/");
+  return data;
+}
+
+export type AppNew = {
+  appSecret: string;
+} & App;
+
+export async function ApplyApp(
+  name: string,
+  callback: string,
+  permitAll: boolean
+): Promise<AppNew> {
+  const {
+    data: { data },
+  } = await apiV1User.post("app/");
+  return data;
+}
