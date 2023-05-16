975fbbfcff8acc477fa611b183fa572e72926a37
Mmx233 <admin@mmxblog.com>
2023-05-16 16:38:05 +0800

    feat: 实装修改应用信息

--- web/src/pages/User/pages/App/components/AppControlBlock/index.tsx
+++ web/src/pages/User/pages/App/components/AppControlBlock/index.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from "react";
 import { useLoadingToast, useMount, useInterval } from "@hooks";
 import toast from "react-hot-toast";
 
+import { AppForm } from "@/pages/User/pages/App/components";
 import { Block } from "@/pages/User/components";
 import { TipIconButton } from "@components";
 import {
@@ -15,13 +16,20 @@ import {
   Stack,
   CircularProgress,
   Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Divider,
 } from "@mui/material";
+import {
+  DeleteForeverOutlined,
+  DriveFileRenameOutlineOutlined,
+} from "@mui/icons-material";
 
-import { DeleteForeverOutlined } from "@mui/icons-material";
+import { GetOwnedAppList, DeleteApp, ModifyApp, App } from "@api/v1/user/app";
 
-import { GetOwnedAppList, DeleteApp, App } from "@api/v1/user/app";
-
-import { useUser } from "@store";
+import { shallow } from "zustand/shallow";
+import { useAppForm, useUser } from "@store";
 
 export const AppControlBlock: FC = () => {
   const apps = useUser((state) => state.apps);
@@ -30,6 +38,27 @@ export const AppControlBlock: FC = () => {
 
   const [onRequestApps, setOnRequestApps] = useState(true);
   const [loadAppsToast, closeAppsToast] = useLoadingToast();
+
+  const [name, callback, permitAll, permitGroups] = useAppForm(
+    (state) => [
+      state.name,
+      state.callback,
+      state.permitAll,
+      state.permitGroups,
+    ],
+    shallow
+  );
+  const [setName, setCallback, setPermitAll, setPermitGroups] = useAppForm(
+    (state) => [
+      state.setState("name"),
+      state.setState("callback"),
+      state.setState("permitAll"),
+      state.setState("permitGroups"),
+    ],
+    shallow
+  );
+  const resetForm = useAppForm((state) => state.reset);
+  const [onModifyApp, setOnModifyApp] = useState<App | null>(null);
 
   async function loadApps() {
     setOnRequestApps(true);
@@ -56,6 +85,50 @@ export const AppControlBlock: FC = () => {
     } catch ({ msg }) {
       if (msg) toast.error(msg as string);
     }
+  }
+
+  async function handleModifyApp() {
+    if (!onModifyApp) return;
+    try {
+      console.log(name);
+      await ModifyApp(
+        onModifyApp.id,
+        name,
+        callback,
+        permitAll,
+        permitGroups?.map((g) => g.id)
+      );
+      toast.success("修改成功");
+      setApps(
+        (apps || []).map((app) =>
+          app.id === onModifyApp.id
+            ? ({
+                id: onModifyApp.id,
+                name: name,
+                callback: callback,
+                permitAllGroup: permitAll,
+                groups: permitGroups || [],
+              } as App)
+            : app
+        )
+      );
+      closeModifyAppDialog();
+    } catch ({ msg }) {
+      if (msg) toast.error(msg as string);
+    }
+  }
+
+  function showModifyAppDialog(app: App) {
+    setName(app.name);
+    setCallback(app.callback);
+    setPermitAll(app.permitAllGroup);
+    setPermitGroups(app.groups);
+    setOnModifyApp(app);
+  }
+
+  function closeModifyAppDialog() {
+    setOnModifyApp(null);
+    resetForm();
   }
 
   useInterval(loadApps, !apps && !onRequestApps ? 2000 : null);
@@ -100,7 +173,25 @@ export const AppControlBlock: FC = () => {
                   </TableCell>
                   <TableCell>{app.callback}</TableCell>
                   <TableCell>
-                    <Stack flexDirection={"row"}>
+                    <Stack
+                      flexDirection={"row"}
+                      alignItems={"center"}
+                      divider={
+                        <Divider
+                          orientation="vertical"
+                          sx={{
+                            height: "15px",
+                            mx: "3px",
+                          }}
+                        />
+                      }
+                    >
+                      <TipIconButton
+                        title={"编辑"}
+                        onClick={() => showModifyAppDialog(app)}
+                      >
+                        <DriveFileRenameOutlineOutlined />
+                      </TipIconButton>
                       <TipIconButton
                         title={"删除"}
                         onClick={() => handleDeleteApp(app)}
@@ -136,6 +227,18 @@ export const AppControlBlock: FC = () => {
           </Stack>
         </TableContainer>
       </Paper>
+
+      <Dialog open={Boolean(onModifyApp)} onClose={closeModifyAppDialog}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <AppForm
+            submitText={"确认"}
+            onSubmit={handleModifyApp}
+            cancelText={"取消"}
+            onCancel={closeModifyAppDialog}
+          />
+        </DialogContent>
+      </Dialog>
     </Block>
   );
 };
