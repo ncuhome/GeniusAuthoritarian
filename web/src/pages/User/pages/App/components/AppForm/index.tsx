f22df71f1b5b9b4d5b700379689394ce70afa4b4
Mmx233 <admin@mmxblog.com>
2023-05-16 15:44:29 +0800

    improve: 加载身份组列表添加失败自动重试

--- web/src/pages/User/pages/App/components/AppForm/index.tsx
+++ web/src/pages/User/pages/App/components/AppForm/index.tsx
@@ -1,5 +1,5 @@
 import { FC, useEffect, useRef, useState } from "react";
-import { useTimeout } from "@hooks";
+import { useTimeout, useLoadingToast, useInterval } from "@hooks";
 import toast from "react-hot-toast";
 
 import { SelectPermitGroup } from "@/pages/User/pages/App/components";
@@ -71,17 +71,19 @@ export const AppForm: FC<Props> = ({
 
   const groups = useGroup((state) => state.groups);
   const setGroups = useGroup((state) => state.setState("groups"));
-  const onRequestGroups = useRef(false);
+  const [onRequestGroups, setOnRequestGroups] = useState(false);
+  const [loadGroupToast, closeLoadGroupToast] = useLoadingToast();
 
   async function loadGroups() {
-    onRequestGroups.current = true;
+    setOnRequestGroups(true);
     try {
       const data = await ListGroups();
       setGroups(data);
+      closeLoadGroupToast();
     } catch ({ msg }) {
-      if (msg) toast.error(msg as string);
+      if (msg) loadGroupToast(msg as string);
     }
-    onRequestGroups.current = false;
+    setOnRequestGroups(false);
   }
 
   async function checkForm(): Promise<boolean> {
@@ -119,97 +121,100 @@ export const AppForm: FC<Props> = ({
     onSubmit();
   }
 
-  useTimeout(() => setShowSelectGroups(false), permitAll ? 300 : null);
-  useEffect(() => {
-    if (!permitAll) {
-      setShowSelectGroups(true);
-      if (onRequestGroups.current || groups) return;
-      loadGroups();
-    }
-  }, [permitAll]);
-  return (
-    <>
-      <Grid container spacing={2} marginTop={0}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            label={"应用名称"}
-            fullWidth
-            inputRef={nameInput}
-            color={nameError ? "error" : "primary"}
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            label={"回调地址"}
-            fullWidth
-            inputRef={callbackInput}
-            color={callbackError ? "error" : "primary"}
-            value={callback}
-            onChange={(e) => {
-              if (e.target.value.indexOf("https://") !== 0) return;
-              setCallback(e.target.value);
+    useTimeout(() => setShowSelectGroups(false), permitAll ? 300 : null);
+    useInterval(loadGroups, !onRequestGroups && !permitAll ? 2000 : null);
+    useEffect(() => {
+      if (!permitAll) {
+        setShowSelectGroups(true);
+        if (onRequestGroups || groups) return;
+        loadGroups();
+      } else {
+        closeLoadGroupToast();
+      }
+    }, [permitAll]);
+    return (
+      <>
+        <Grid container spacing={2} marginTop={0}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label={"应用名称"}
+              fullWidth
+              inputRef={nameInput}
+              color={nameError ? "error" : "primary"}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label={"回调地址"}
+              fullWidth
+              inputRef={callbackInput}
+              color={callbackError ? "error" : "primary"}
+              value={callback}
+              onChange={(e) => {
+                if (e.target.value.indexOf("https://") !== 0) return;
+                setCallback(e.target.value);
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={permitAll}
+                  onChange={(e) => setPermitAll(e.target.checked)}
+                />
+              }
+              label="允许所有成员使用"
+            />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            sx={{
+              transition: `padding .3s ease-out${
+                permitAll ? " .3s" : ""
+              }, opacity 0.3s ease-out${permitAll ? "" : " .3s"}`,
+              py: permitAll ? "0!important" : undefined,
+              opacity: permitAll ? "0" : undefined,
             }}
-          />
+          >
+            <Collapse in={showSelectGroups}>
+              {groups ? (
+                <SelectPermitGroup
+                  groups={groups}
+                  permitGroups={permitGroups}
+                  setPermitGroups={setPermitGroups}
+                  fullWidth
+                />
+              ) : (
+                <Skeleton variant={"rounded"} width={"100%"} height={56} />
+              )}
+            </Collapse>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={permitAll}
-                onChange={(e) => setPermitAll(e.target.checked)}
-              />
-            }
-            label="允许所有成员使用"
-          />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={6}
+
+        <Stack
+          flexDirection={"row"}
+          justifyContent={"flex-end"}
+          flexWrap={"wrap"}
           sx={{
-            transition: `padding .3s ease-out${
-              permitAll ? " .3s" : ""
-            }, opacity 0.3s ease-out${permitAll ? "" : " .3s"}`,
-            py: permitAll ? "0!important" : undefined,
-            opacity: permitAll ? "0" : undefined,
+            marginTop: "1rem",
+            "&>button": {
+              marginLeft: "0.8rem",
+            },
           }}
         >
-          <Collapse in={showSelectGroups}>
-            {groups ? (
-              <SelectPermitGroup
-                groups={groups}
-                permitGroups={permitGroups}
-                setPermitGroups={setPermitGroups}
-                fullWidth
-              />
-            ) : (
-              <Skeleton variant={"rounded"} width={"100%"} height={56} />
-            )}
-          </Collapse>
-        </Grid>
-      </Grid>
-
-      <Stack
-        flexDirection={"row"}
-        justifyContent={"flex-end"}
-        flexWrap={"wrap"}
-        sx={{
-          marginTop: "1rem",
-          "&>button": {
-            marginLeft: "0.8rem",
-          },
-        }}
-      >
-        <Button variant={"outlined"} onClick={onCancel}>
-          {cancelText}
-        </Button>
-        <Button variant={"contained"} onClick={handleSubmit}>
-          {submitText}
-        </Button>
-      </Stack>
-    </>
-  );
+          <Button variant={"outlined"} onClick={onCancel}>
+            {cancelText}
+          </Button>
+          <Button variant={"contained"} onClick={handleSubmit}>
+            {submitText}
+          </Button>
+        </Stack>
+      </>
+    );
 };
 export default AppForm;
