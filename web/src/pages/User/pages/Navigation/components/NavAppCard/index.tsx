96156a31e21a35f63a4fcfc34dcbcd3187a3e38d
Mmx233 <admin@mmxblog.com>
2023-05-18 15:10:40 +0800

    feat: 实装一键登录应用

--- web/src/pages/User/pages/Navigation/components/NavAppCard/index.tsx
+++ web/src/pages/User/pages/Navigation/components/NavAppCard/index.tsx
@@ -1,14 +1,9 @@
 import { FC, useState } from "react";
+import toast from "react-hot-toast";
 
-import {
-  Card,
-  CardContent,
-  Typography,
-  CardActions,
-  Button,
-} from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
 
-import { App } from "@api/v1/user/app";
+import { App, GetLandingUrl } from "@api/v1/user/app";
 
 interface Props {
   app: App;
@@ -17,12 +12,22 @@ interface Props {
 export const NavAppCard: FC<Props> = ({ app }) => {
   const [elevation, setElevation] = useState(5);
 
+  async function LandingApp(id: number) {
+    try {
+      const url = await GetLandingUrl(id);
+      window.open(url, "_blank");
+    } catch ({ msg }) {
+      if (msg) toast.error(msg as string);
+    }
+  }
+
   return (
     <Card
       variant={"elevation"}
       elevation={elevation}
       onMouseEnter={() => setElevation(7)}
       onMouseLeave={() => setElevation(5)}
+      onClick={() => LandingApp(app.id)}
       sx={{
         transition: "box-shadow .3s ease-in-out",
       }}
