bf05fa0df31e8663e8c74bb779114e32e66e5344
Mmx233 <admin@mmxblog.com>
2023-06-06 16:30:07 +0800

    improve: profile 接口加入是否已开启 2fa

--- internal/db/dto/User.go
+++ internal/db/dto/User.go
@@ -4,5 +4,6 @@ type UserProfile struct {
 	ID     uint    `json:"id"`
 	Name   string  `json:"name"`
 	Phone  string  `json:"phone"`
+	MFA    bool    `json:"mfa"`
 	Groups []Group `json:"groups" gorm:"-"`
 }
