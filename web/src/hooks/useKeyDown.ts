9c8fe2dd7d650b0fc7052cf7a06e371ddd2a471d
Mmx233 <admin@mmxblog.com>
2023-06-07 10:28:58 +0800

    fix: 一处遗漏的导入修改

--- web/src/hooks/useKeyDown.ts
+++ web/src/hooks/useKeyDown.ts
@@ -1,5 +1,4 @@
-import { useEffect, useRef } from "react";
-import { useEventListener } from ".";
+import { useEventListener } from "@hooks/useEventListener";
 
 export const useKeyDown = (key: string | null, callback: () => void) => {
   useEventListener(key ? "keydown" : null, (e: KeyboardEvent) =>
