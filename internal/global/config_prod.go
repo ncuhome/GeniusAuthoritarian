b9c8c31a0b7731c191d33e02f6966782bfdcbc44
Mmx233 <admin@mmxblog.com>
2023-07-04 16:57:58 +0800

    improve: 修改 gin release mode 的进入逻辑

--- internal/global/config_prod.go
+++ internal/global/config_prod.go
@@ -4,10 +4,8 @@ package global
 
 import (
 	"github.com/Mmx233/EnvConfig"
-	"github.com/gin-gonic/gin"
 )
 
 func initConfig() {
 	EnvConfig.Load("", &Config)
-	gin.SetMode(gin.ReleaseMode)
 }
---
+++ internal/router/router.go
@@ -0,0 +1,12 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ncuhome/GeniusAuthoritarian/internal/global"
+)
+
+func init() {
+	if !global.Config.TraceMode {
+		gin.SetMode(gin.ReleaseMode)
+	}
+}
