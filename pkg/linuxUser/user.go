57b2fe4ddb6d91fba9f1d6f52374407547c03455
Mmx233 <admin@mmxblog.com>
2023-08-03 00:25:39 +0800

    feat: linuxUser 操作包

---
+++ pkg/linuxUser/user.go
@@ -0,0 +1,45 @@
+package linuxUser
+
+import (
+	"github.com/Mmx233/tool"
+	"os"
+	"os/exec"
+	"path"
+)
+
+func UserHomePath(username string) string {
+	return "/home/" + username
+}
+
+func Exist(username string) (bool, error) {
+	return tool.File.Exists(UserHomePath(username))
+}
+
+func Create(username string) error {
+	return exec.Command("useradd", "-m", "-s", "/bin/sh", username).Run()
+}
+
+func Delete(username string) error {
+	return exec.Command("userdel", "-r", username).Run()
+}
+
+func PrepareSshDir(username string) error {
+	dirPath := path.Join(UserHomePath(username), ".ssh")
+	if exist, err := tool.File.Exists(dirPath); err != nil {
+		return err
+	} else if !exist {
+		return os.Mkdir(dirPath, 700)
+	}
+	return nil
+}
+
+func WriteAuthorizedKeys(username, publicKey string) error {
+	f, err := os.OpenFile(path.Join(UserHomePath(username), ".ssh", "authorized_keys"), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 400)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(publicKey)
+	return err
+}
