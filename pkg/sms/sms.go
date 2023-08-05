cd7dfed2a2f212d132d2be6d1d1c79aaebba5fd7
Mmx233 <admin@mmxblog.com>
2023-08-05 17:10:01 +0800

    refactor: 使用 encoding transform 重构 sms 模块

--- pkg/sms/sms.go
+++ pkg/sms/sms.go
@@ -1,11 +1,12 @@
 package sms
 
 import (
+	"bytes"
 	"errors"
-	"fmt"
 	"github.com/Mmx233/tool"
-	"github.com/ncuhome/urlencode"
-	"net/http"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"io"
 	"net/url"
 	"time"
 )
@@ -22,25 +23,37 @@ type Ums struct {
 	conf UmsConf
 }
 
+func (a Ums) transformEncoding(i io.Reader, encoder transform.Transformer) (string, error) {
+	reader := transform.NewReader(i, encoder)
+	d, err := io.ReadAll(reader)
+	return string(d), err
+}
+
 func (a Ums) Send(msg string, phone string) error {
-	var e error
-	msg, e = urlencode.Encode(msg, "gbk")
-	if e != nil {
-		return e
+	msgGbk, err := a.transformEncoding(bytes.NewBuffer([]byte(msg)), simplifiedchinese.GBK.NewEncoder())
+
+	res, err := a.http.GetRequest(&tool.DoHttpReq{
+		Url: "http://smsapi.ums86.com:8888/sms/Api/Send.do",
+		Query: map[string]interface{}{
+			"SpCode":         a.conf.SpCode,
+			"LoginName":      a.conf.LoginName,
+			"Password":       a.conf.Password,
+			"MessageContent": msgGbk,
+			"UserNumber":     phone,
+			"SerialNumber":   time.Now().UnixNano(),
+		},
+	})
+	if err != nil {
+		return err
 	}
-	if req, e := http.Post(
-		fmt.Sprintf(
-			"https://smsapi.ums86.com:8888/sms/Api/Send.do?SpCode=%s&LoginName=%s&Password=%s&MessageContent=%s&UserNumber=%s&SerialNumber=%v",
-			a.conf.SpCode, a.conf.LoginName, a.conf.Password, msg, phone, time.Now().UnixNano()),
-		"application/x-www-form-urlencoded", nil); e != nil {
-		return e
-	} else if data, e := a.http.ReadResBodyToString(req.Body); e != nil {
-		return e
-	} else if res, e := url.ParseQuery(data); e != nil {
-		return e
-	} else if res.Get("result") != "0" {
-		data, _ = urlencode.Decode(data, "gbk")
-		return errors.New(data)
+
+	defer res.Body.Close()
+	resStr, err := a.transformEncoding(res.Body, simplifiedchinese.GBK.NewDecoder())
+	data, err := url.ParseQuery(resStr)
+	if err != nil {
+		return err
+	} else if data.Get("result") != "0" {
+		return errors.New(resStr)
 	}
 
 	return nil
