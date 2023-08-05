33391811fc79b2bacf03d177bc881267ac44105d
Mmx233 <admin@mmxblog.com>
2023-08-05 22:29:23 +0800

    feat: NewMfaForm zustand

---
+++ web/src/store/useNewMfa.ts
@@ -0,0 +1,32 @@
+import {create, StoreApi, UseBoundStore} from "zustand";
+
+interface NewMfaForm {
+    step: number
+    smsCode: string
+    mfaCode: string
+}
+
+interface NewMfaAction {
+    reset: () => void;
+    setState: <T extends keyof NewMfaForm>(
+        key: T
+    ) => (value: NewMfaForm[T]) => void;
+}
+
+const initialMfaForm :NewMfaForm = {
+    step: 0,
+    smsCode: "",
+    mfaCode: ""
+}
+
+export type UseAppForm = UseBoundStore<StoreApi<NewMfaForm & NewMfaAction>>;
+
+const useNewMfaForm = create<NewMfaForm & NewMfaAction>()((set) => ({
+    ...initialMfaForm,
+
+    reset: () => {
+        set(initialMfaForm);
+    },
+    setState: (key) => (value) => set({ [key]: value }),
+}));
+export default useNewMfaForm
