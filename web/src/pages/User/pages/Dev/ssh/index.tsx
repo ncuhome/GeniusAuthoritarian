845c04aa7ac725690b1bbead8ab009f5a49d8e6b
Mmx233 <admin@mmxblog.com>
2023-08-06 01:20:23 +0800

    feat: user dev ssh 页面锁定状态样式

--- web/src/pages/User/pages/Dev/ssh/index.tsx
+++ web/src/pages/User/pages/Dev/ssh/index.tsx
@@ -1,6 +1,34 @@
 import { FC } from "react";
 
+import { Stack } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { LockPerson, Link } from "@mui/icons-material";
+import Block from "@components/user/Block";
+
 const Ssh: FC = () => {
-  return <></>;
+  return (
+    <Block>
+      <Stack alignItems={"center"} justifyContent={"center"} height={"10rem"}>
+        <Stack
+          justifyContent={"center"}
+          alignItems={"center"}
+          flexDirection={"row"}
+          mb={"1rem"}
+        >
+          <Link color={"disabled"} />
+          <LockPerson
+            sx={{
+              fontSize: "3rem",
+              paddingBottom: "0.55rem",
+              paddingX: "0.5rem",
+            }}
+          />
+          <Link color={"disabled"} />
+        </Stack>
+
+        <LoadingButton variant={"outlined"}>解锁</LoadingButton>
+      </Stack>
+    </Block>
+  );
 };
 export default Ssh;
