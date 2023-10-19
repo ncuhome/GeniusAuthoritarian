aa0f0d8e08e13c0c9a663b7228b3603307d92499
Mmx233 <admin@mmxblog.com>
2023-10-19 21:45:00 +0800

    feat: redis passkey session store 添加 user overload

--- internal/db/redis/Passkey.go
+++ internal/db/redis/Passkey.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"time"
 )
 
@@ -14,22 +15,50 @@ type Passkey struct {
 }
 
 func (p Passkey) key(ip string) string {
-	return keyPasskey.String() + "ip-" + ip
+	return keyPasskey.String() + "ip" + ip
 }
 
-func (p Passkey) StoreSession(ctx context.Context, ip string, session any, expire time.Duration) error {
+func (p Passkey) store(ctx context.Context, key string, session any, expire time.Duration) error {
 	data, err := json.Marshal(session)
 	if err != nil {
 		return err
 	}
-	return Client.Set(ctx, p.key(ip), data, expire).Err()
+	return Client.Set(ctx, key, data, expire).Err()
 }
 
-// ReadSession 读取后自动销毁
-func (p Passkey) ReadSession(ctx context.Context, ip string, session any) error {
-	data, err := Client.GetDel(ctx, p.key(ip)).Bytes()
+func (p Passkey) StoreSession(ctx context.Context, ip string, session any, expire time.Duration) error {
+	return p.store(ctx, p.key(ip), session, expire)
+}
+
+func (p Passkey) read(ctx context.Context, key string, session any) error {
+	data, err := Client.GetDel(ctx, key).Bytes()
 	if err != nil {
 		return err
 	}
 	return json.Unmarshal(data, session)
+}
+
+// ReadSession 读取后自动销毁
+func (p Passkey) ReadSession(ctx context.Context, ip string, session any) error {
+	return p.read(ctx, p.key(ip), session)
+}
+
+func (p Passkey) User(id uint) UserPasskey {
+	return UserPasskey{
+		p:   p,
+		key: keyPasskey.String() + "u" + fmt.Sprint(id),
+	}
+}
+
+type UserPasskey struct {
+	p   Passkey
+	key string
+}
+
+func (u UserPasskey) StoreSession(ctx context.Context, session any, expire time.Duration) error {
+	return u.p.store(ctx, u.key, session, expire)
+}
+
+func (u UserPasskey) ReadSession(ctx context.Context, session any) error {
+	return u.p.read(ctx, u.key, session)
 }
