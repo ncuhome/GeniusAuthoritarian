9f0bf3bc84d2a3b10647f1445d1a63925e9c5f54
Mmx233 <admin@mmxblog.com>
2023-10-21 15:45:52 +0800

    feat: 添加通行密钥时如果双因素启用则要求验证

--- internal/api/router/user/passkey/registor.go
+++ internal/api/router/user/passkey/registor.go
@@ -3,9 +3,10 @@ package passkey
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/ncuhome/GeniusAuthoritarian/internal/api/controllers/user/passkey"
+	"github.com/ncuhome/GeniusAuthoritarian/internal/api/middlewares"
 )
 
 func routerRegister(G *gin.RouterGroup) {
-	G.GET("/", controllers.BeginPasskeyRegistration)
+	G.GET("/", middlewares.ShouldMfa, controllers.BeginPasskeyRegistration)
 	G.POST("/", controllers.FinishPasskeyRegistration)
 }
