c1138e68de224abd45db0998bffff8d4999a73c9
Mmx233 <admin@mmxblog.com>
2023-11-22 22:34:42 +0800

    improve: dev/ssh 未解锁样式及解锁动画

--- web/src/pages/User/pages/Dev/ssh/index.tsx
+++ web/src/pages/User/pages/Dev/ssh/index.tsx
@@ -1,17 +1,7 @@
 import { FC, useState } from "react";
 import toast from "react-hot-toast";
 
-import {
-  Stack,
-  TextField,
-  Select,
-  MenuItem,
-  FormControl,
-  InputLabel,
-  ButtonGroup,
-  Fade,
-  Box,
-} from "@mui/material";
+import { Stack, ButtonGroup, Fade, Box } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import {
   LockPerson,
@@ -19,6 +9,7 @@ import {
   Lock,
   RestartAlt,
   PersonOff,
+  Fingerprint,
 } from "@mui/icons-material";
 import Block from "@components/user/Block";
 import TipButton from "@components/TipButton";
@@ -28,6 +19,26 @@ import KeyPair from "@components/user/dev/ssh/KeyPair";
 import { apiV1User } from "@api/v1/user/base";
 
 import useU2F from "@hooks/useU2F";
+
+const invalidKey: User.SSH.Keys = {
+  username: "114514",
+  ssh: {
+    public:
+      "ssh-ed25519 F1WfOB4G90IdPFsJPr3XhZSupRSSJJ2MFfPxL081t5qEdLxrOJjdq70ou27PzazCCnwX",
+    private:
+      "-----BEGIN OPENSSH PRIVATE KEY-----\n" +
+      "L0RtR+88cLywV7W4cMVvE4p/iIcIuyRx0rN0ViIRVcykH9U5DbU/+CEML+n0hPGh\n" +
+      "fdA2hhLT7+0i4uh9VDmm9MnUrEzpkUNlRgOhlwZdOMyjnhDu8CQ6ARG9+6h0QPMr\n" +
+      "UIO7na+qmJvR0CuY4GQj9Xxlla/68k0YXopE/tn+SvpVuLX79PzcvfMQh7Jn0Xyo\n" +
+      "0sjCZUOWkKBBWTw7B/Lzl79FD3R+EAdz6uY7m9ulrk4Tsg9MQ0BjImgzfDA/qh4N\n" +
+      "0YeWAr8y2qNrVE/ddsrrER6VR5zKRekp1jZcVFd9QFbwMZU0MaBYJ+1a\n" +
+      "-----END OPENSSH PRIVATE KEY-----",
+  },
+  pem: {
+    public: "",
+    private: "",
+  },
+};
 
 const Ssh: FC = () => {
   const { isLoading: u2fLoading, refreshToken } = useU2F();
@@ -114,46 +125,63 @@ const Ssh: FC = () => {
           </Fade>
         </Stack>
 
-        {sshKey ? (
+        <Box sx={{ position: "relative" }}>
           <KeyPair
             spacing={3}
             py={2}
-            keyMode={keyMode}
-            keys={sshKey}
+            keyMode={sshKey ? keyMode : "ssh"}
+            keys={sshKey ?? invalidKey}
             onSetKeyMode={setKeyMode}
+            sx={
+              sshKey
+                ? { transition: "filter 0.25s" }
+                : {
+                    filter: "blur(4.5px)",
+                  }
+            }
           />
-        ) : (
-          <Stack
-            alignItems={"center"}
-            justifyContent={"center"}
-            height={"15rem"}
-          >
-            <Stack
-              justifyContent={"center"}
-              alignItems={"center"}
-              flexDirection={"row"}
-              mb={"1rem"}
-            >
-              <Link color={"disabled"} />
-              <LockPerson
-                sx={{
-                  fontSize: "3rem",
-                  paddingBottom: "0.55rem",
-                  paddingX: "0.5rem",
-                }}
-              />
-              <Link color={"disabled"} />
-            </Stack>
 
-            <LoadingButton
-              variant={"outlined"}
-              loading={isUnlockLoading}
-              onClick={onShowSshKeys}
+          <Fade in={!sshKey} appear={false}>
+            <Stack
+              alignItems={"center"}
+              justifyContent={"center"}
+              sx={{
+                position: "absolute",
+                left: 0,
+                top: 0,
+                bottom: 0,
+                right: 0,
+              }}
             >
-              解锁
-            </LoadingButton>
-          </Stack>
-        )}
+              <Stack
+                justifyContent={"center"}
+                alignItems={"center"}
+                flexDirection={"row"}
+                mb={"1rem"}
+              >
+                <Link fontSize={"large"} />
+                <LockPerson
+                  sx={{
+                    fontSize: "4rem",
+                    paddingBottom: "0.75rem",
+                    paddingX: "1rem",
+                  }}
+                />
+                <Link fontSize={"large"} />
+              </Stack>
+
+              <LoadingButton
+                variant={"contained"}
+                color={"secondary"}
+                loading={isUnlockLoading}
+                onClick={onShowSshKeys}
+                startIcon={<Fingerprint />}
+              >
+                解锁
+              </LoadingButton>
+            </Stack>
+          </Fade>
+        </Box>
       </Stack>
     </Block>
   );
