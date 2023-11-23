af7b3341e592b8fec1cdb71b13fd9b1b70b720d6
Mmx233 <admin@mmxblog.com>
2023-11-23 22:55:45 +0800

    chore: 移除 landing 接口

--- internal/api/router/user/app.go
+++ internal/api/router/user/app.go
@@ -2,10 +2,9 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
-	controllers2 "github.com/ncuhome/GeniusAuthoritarian/internal/api/controllers/user/dev/app"
+	"github.com/ncuhome/GeniusAuthoritarian/internal/api/controllers/user/dev/app"
 )
 
 func routerApp(G *gin.RouterGroup) {
-	G.GET("/accessible", controllers2.ListAccessibleApp)
-	G.GET("/landing", controllers2.LandingApp)
+	G.GET("/accessible", controllers.ListAccessibleApp)
 }
