6bb2000b5cd7fed9e5031c1e103f840af989d6ea
Mmx233 <admin@mmxblog.com>
2023-11-25 16:39:51 +0800

    improve: 优化导航计数小数显示

--- web/src/util/num.ts
+++ web/src/util/num.ts
@@ -3,7 +3,14 @@ export function numeral(num: number, digital = 1): string {
     return String(num);
   }
   if (num < 1000 * 1000) {
-    return (num / 1000).toFixed(digital) + "k";
+    num = num / 1000;
+    let value: string;
+    if (num < 1000 * 100) {
+      value = num.toFixed(digital);
+    } else {
+      value = num.toFixed(0);
+    }
+    return value + "k";
   }
   return (num / 1000 / 1000).toFixed(digital) + "m";
 }
