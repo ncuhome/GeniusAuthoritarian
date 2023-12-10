470ba524603701f426887e39d708b50e9520561e
Mmx233 <admin@mmxblog.com>
2023-12-10 19:20:16 +0800

    fix: 更换 babel import plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -13,22 +13,8 @@ export default defineConfig({
       babel: {
         plugins: [
           [
-            "babel-plugin-import",
-            {
-              libraryName: "@mui/material",
-              libraryDirectory: "",
-              camel2DashComponentName: false,
-            },
-            "core",
-          ],
-          [
-            "babel-plugin-import",
-            {
-              libraryName: "@mui/icons-material",
-              libraryDirectory: "",
-              camel2DashComponentName: false,
-            },
-            "icons",
+            "babel-plugin-direct-import",
+            { modules: ["@mui/material", "@mui/icons-material"] },
           ],
         ],
       },
