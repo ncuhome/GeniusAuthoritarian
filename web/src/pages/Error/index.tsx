886cf0293cc47e9e4c5dd15f597d56949e1c7850
Mmx233 <admin@mmxblog.com>
2023-12-13 20:50:46 +0800

    improve: 错误页 UI

--- web/src/pages/Error/index.tsx
+++ web/src/pages/Error/index.tsx
@@ -1,9 +1,10 @@
 import { FC, useMemo } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import "./styles.css";
 
-import { Box, Typography, ButtonGroup, Button } from "@mui/material";
-import { ClearRounded } from "@mui/icons-material";
+import img_png from "@/assets/img/error/img_20231213.png";
+import img_webp from "@/assets/img/error/img_20231213.webp";
+
+import { Box, Stack, Typography, ButtonGroup, Button } from "@mui/material";
 
 import { GoLogin } from "@util/nav";
 
@@ -27,78 +28,92 @@ export const Error: FC = () => {
       <Box
         sx={{
           display: "flex",
-          flexDirection: "column",
-          textAlign: "center",
-          justifyContent: "center",
-          "&>*": {
-            marginBottom: "1rem!important",
-          },
+          flexDirection: { xs: "column", sm: "row" },
+          alignItems: "center",
         }}
       >
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            "& svg": {
-              borderStyle: "solid",
-              borderRadius: "50%",
-              fontSize: "8rem",
-              animation: "error-page-fork .5s ease",
-              animationFillMode: "forwards",
-              boxSizing: "border-box",
+            maxWidth: "40%",
+            width: "22rem",
+            margin: { xs: "0 0 1.2rem 0", sm: "0 5rem 0 0" },
+            "&>picture": {
+              height: "auto",
+              width: "100%",
+              "&>img": {
+                width: "100%",
+                height: "auto",
+              },
             },
           }}
         >
-          <ClearRounded />
+          <picture>
+            <source srcSet={img_webp} type="image/webp" />
+            <img src={img_png} alt={"ERROR"} />
+          </picture>
         </Box>
-        <Typography
-          variant={"h4"}
-          sx={{
-            fontWeight: 600,
-            letterSpacing: "0.25rem",
-          }}
-        >
-          {title}
-        </Typography>
-        {state?.content ? (
+
+        <Stack alignItems={{ xs: "center", sm: "baseline" }}>
           <Typography
-            sx={{
-              color: "#999",
-              wordBreak: "break-all",
-            }}
+            variant={"h1"}
+            fontWeight={"bolder"}
+            letterSpacing={"0.2rem"}
+            mb={4}
+            color={"text.secondary"}
+            display={{ xs: "none", sm: "block" }}
           >
-            {state.content}
+            OPS!
           </Typography>
-        ) : undefined}
 
-        <Box mt={3.5}>
-          <ButtonGroup
-            variant="text"
-            sx={{
-              "&>button": {
-                border: "unset!important",
-              },
-            }}
-          >
-            <Button
-              onClick={() =>
-                window.open(
-                  "https://applink.feishu.cn/client/chat/chatter/add_by_link?link_token=250la2bb-add8-4fde-ac94-57376aee2e40",
-                  "_blank"
-                )
-              }
+          <Stack px={0.7}>
+            <Typography
+              variant={"h4"}
+              fontWeight={"bold"}
+              letterSpacing={"0.25rem"}
+              mb={1}
             >
-              反馈
-            </Button>
-
-            {state?.retryAppCode !== undefined ? (
-              <Button onClick={() => GoLogin(nav, state!.retryAppCode!)}>
-                重试
-              </Button>
+              {title}
+            </Typography>
+            {state?.content ? (
+              <Typography
+                sx={{
+                  color: "#999",
+                  wordBreak: "break-all",
+                }}
+              >
+                {state.content}
+              </Typography>
             ) : undefined}
-          </ButtonGroup>
-        </Box>
+          </Stack>
+
+          <Box mt={1.5}>
+            <ButtonGroup
+              variant="text"
+              sx={{
+                "&>button": {
+                  border: "unset!important",
+                },
+              }}
+            >
+              <Button
+                onClick={() =>
+                  window.open(
+                    "https://applink.feishu.cn/client/chat/chatter/add_by_link?link_token=250la2bb-add8-4fde-ac94-57376aee2e40",
+                    "_blank",
+                  )
+                }
+              >
+                反馈
+              </Button>
+
+              {state?.retryAppCode !== undefined ? (
+                <Button onClick={() => GoLogin(nav, state!.retryAppCode!)}>
+                  重试
+                </Button>
+              ) : undefined}
+            </ButtonGroup>
+          </Box>
+        </Stack>
       </Box>
     </Box>
   );
