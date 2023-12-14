818aa19ecb96ef388066294f0a9a2db6534c8ef9
Mmx233 <admin@mmxblog.com>
2023-12-14 18:42:05 +0800

    feat: TokenStore 支持 CreateStorePointWithID

--- pkg/tokenStore/tokenStore.go
+++ pkg/tokenStore/tokenStore.go
@@ -28,24 +28,26 @@ func (a TokenStore[C]) genKey(id uint64) string {
 	return a.keyPrefix + fmt.Sprint(id)
 }
 
-func (a TokenStore[C]) CreateStorePoint(ctx context.Context, valid time.Duration, claims *C) (uint64, error) {
+func (a TokenStore[C]) CreateStorePointWithID(ctx context.Context, id uint64, valid time.Duration, claims *C) error {
 	var value []byte
 	var err error
 	if claims != nil {
 		value, err = json.Marshal(claims)
 		if err != nil {
-			return 0, err
+			return err
 		}
 	} else {
 		value = []byte{'1'}
 	}
+	return a.client.Set(ctx, a.genKey(id), value, valid).Err()
+}
 
+func (a TokenStore[C]) CreateStorePoint(ctx context.Context, valid time.Duration, claims *C) (uint64, error) {
 	id, err := a.client.Incr(ctx, a.keyID).Uint64()
 	if err != nil {
 		return 0, err
 	}
-
-	return id, a.client.Set(ctx, a.genKey(id), value, valid).Err()
+	return id, a.CreateStorePointWithID(ctx, id, valid, claims)
 }
 
 func (a TokenStore[C]) NewStorePoint(id uint64) Point[C] {
