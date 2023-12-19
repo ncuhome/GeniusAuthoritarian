38b31c54042cf06417a2b8ac5bda1a2211b9d080
Mmx233 <admin@mmxblog.com>
2023-12-19 11:27:50 +0800

    fix: refreshToken rpc 未校验 appCode

---
+++ internal/rpc/refreshToken/auth.go
@@ -0,0 +1,59 @@
+package refreshToken
+
+import (
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"github.com/ncuhome/GeniusAuthoritarian/internal/pkg/jwt"
+	"github.com/ncuhome/GeniusAuthoritarian/internal/pkg/jwt/jwtClaims"
+	"github.com/ncuhome/GeniusAuthoritarian/internal/service"
+	refreshTokenProto "github.com/ncuhome/GeniusAuthoritarianProtos/refreshToken"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"gorm.io/gorm"
+	"unsafe"
+)
+
+func CheckSignature(req *refreshTokenProto.TokenRequest) error {
+	_, appSecret, err := service.App.FirstAppKeyPairByAppCode(req.AppCode)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return status.Error(codes.Unauthenticated, "appCode not found")
+		}
+		return status.Error(codes.Internal, "database error")
+	}
+
+	signStr := fmt.Sprintf("%s:%s:%s", req.AppCode, appSecret, req.Token)
+	h := sha256.New()
+	h.Write(unsafe.Slice(unsafe.StringData(signStr), len(signStr)))
+	if req.Signature != fmt.Sprintf("%x", h.Sum(nil)) {
+		return status.Error(codes.Unauthenticated, "signature invalid")
+	}
+	return nil
+}
+
+func CheckRefreshToken(req *refreshTokenProto.TokenRequest) (*jwtClaims.RefreshToken, error) {
+	err := CheckSignature(req)
+	if err != nil {
+		return nil, err
+	}
+
+	claims, valid, err := jwt.ParseRefreshToken(req.Token)
+	if err != nil || !valid || claims.AppCode != req.AppCode {
+		return nil, status.Error(codes.Unauthenticated, "token invalid")
+	}
+	return claims, nil
+}
+
+func CheckAccessToken(req *refreshTokenProto.TokenRequest) (*jwtClaims.AccessToken, error) {
+	err := CheckSignature(req)
+	if err != nil {
+		return nil, err
+	}
+
+	claims, valid, err := jwt.ParseAccessToken(req.Token)
+	if err != nil || !valid || claims.AppCode != req.AppCode {
+		return nil, status.Error(codes.Unauthenticated, "token invalid")
+	}
+	return claims, nil
+}
--- internal/rpc/refreshToken/server.go
+++ internal/rpc/refreshToken/server.go
@@ -2,9 +2,6 @@ package refreshToken
 
 import (
 	"context"
-	"crypto/sha256"
-	"errors"
-	"fmt"
 	"github.com/ncuhome/GeniusAuthoritarian/internal/db/redis"
 	"github.com/ncuhome/GeniusAuthoritarian/internal/pkg/jwt"
 	"github.com/ncuhome/GeniusAuthoritarian/internal/rpc/middlewares"
@@ -14,8 +11,6 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
-	"gorm.io/gorm"
-	"unsafe"
 )
 
 func NewRpc() *grpc.Server {
@@ -35,33 +30,10 @@ type Server struct {
 	refreshTokenProto.UnimplementedRefreshTokenServer
 }
 
-func (s *Server) CheckSignature(req *refreshTokenProto.TokenRequest) error {
-	_, appSecret, err := service.App.FirstAppKeyPairByAppCode(req.AppCode)
-	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return status.Error(codes.Unauthenticated, "appCode not found")
-		}
-		return status.Error(codes.Internal, "database error")
-	}
-
-	signStr := fmt.Sprintf("%s:%s:%s", req.AppCode, appSecret, req.Token)
-	h := sha256.New()
-	h.Write(unsafe.Slice(unsafe.StringData(signStr), len(signStr)))
-	if req.Signature != fmt.Sprintf("%x", h.Sum(nil)) {
-		return status.Error(codes.Unauthenticated, "signature invalid")
-	}
-	return nil
-}
-
 func (s *Server) RefreshToken(_ context.Context, req *refreshTokenProto.TokenRequest) (*refreshTokenProto.AccessToken, error) {
-	err := s.CheckSignature(req)
+	claims, err := CheckRefreshToken(req)
 	if err != nil {
 		return nil, err
-	}
-
-	claims, valid, err := jwt.ParseRefreshToken(req.Token)
-	if err != nil || !valid {
-		return nil, status.Error(codes.Unauthenticated, "token invalid")
 	}
 
 	accessToken, err := jwt.GenerateAccessToken(claims.ID, claims.UID, req.AppCode, claims.Payload)
@@ -76,14 +48,9 @@ func (s *Server) RefreshToken(_ context.Context, req *refreshTokenProto.TokenReq
 }
 
 func (s *Server) DestroyRefreshToken(_ context.Context, req *refreshTokenProto.TokenRequest) (*emptypb.Empty, error) {
-	err := s.CheckSignature(req)
+	claims, err := CheckRefreshToken(req)
 	if err != nil {
 		return nil, err
-	}
-
-	claims, valid, err := jwt.ParseRefreshToken(req.Token)
-	if err != nil || !valid {
-		return nil, status.Error(codes.Unauthenticated, "token invalid")
 	}
 
 	loginRecordSrv, err := service.LoginRecord.Begin()
@@ -110,14 +77,9 @@ func (s *Server) DestroyRefreshToken(_ context.Context, req *refreshTokenProto.T
 }
 
 func (s *Server) VerifyAccessToken(_ context.Context, req *refreshTokenProto.TokenRequest) (*refreshTokenProto.AccessTokenInfo, error) {
-	err := s.CheckSignature(req)
+	claims, err := CheckAccessToken(req)
 	if err != nil {
 		return nil, err
-	}
-
-	claims, valid, err := jwt.ParseAccessToken(req.Token)
-	if err != nil || !valid {
-		return nil, status.Error(codes.Unauthenticated, "token invalid")
 	}
 
 	return &refreshTokenProto.AccessTokenInfo{
@@ -127,14 +89,9 @@ func (s *Server) VerifyAccessToken(_ context.Context, req *refreshTokenProto.Tok
 }
 
 func (s *Server) GetUserInfo(_ context.Context, req *refreshTokenProto.TokenRequest) (*refreshTokenProto.UserInfo, error) {
-	err := s.CheckSignature(req)
+	claims, err := CheckAccessToken(req)
 	if err != nil {
 		return nil, err
-	}
-
-	claims, valid, err := jwt.ParseAccessToken(req.Token)
-	if err != nil || !valid {
-		return nil, status.Error(codes.Unauthenticated, "token invalid")
 	}
 
 	user, err := service.User.UserInfoByID(claims.UID)
